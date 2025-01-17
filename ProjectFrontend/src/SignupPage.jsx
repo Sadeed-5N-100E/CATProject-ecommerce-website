3dcec091027006e6d0405a9610eea4a207f2b13f
ShanHangTan <tanshanhang@gmail.com>
2025-01-18 01:26:12 +0800

    Sign Up in Progress and Updated Login Page

--- ProjectFrontend/src/SignupPage.jsx
+++ ProjectFrontend/src/SignupPage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React , {useState} from 'react';
+import { Link } from 'react-router-dom';
+import "./SignupPage.css";
 
 const SignupPage = () => {
+
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneno, setPhoneno] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmpassword, setConfirmpassword] = useState('');
+
   return (
-    <div>SignupPage</div>
+    <div className="SignupBG">
+      <article className="SignupArticle">
+        <h1>SIGN UP NOW</h1>
+      </article>
+    </div>
   )
 }
 
-export default SignupPage
+export default SignupPage;
