3c655abbc727d2fedba2476fe365acde5369890d
L0L-1P0P <156730335+L0L-1P0P@users.noreply.github.com>
2025-01-18 02:07:32 +0800

    save latestversion

--- ProjectFrontend/src/ViewcartPage.jsx
+++ ProjectFrontend/src/ViewcartPage.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-
+import React, { useState ,useEffect} from 'react';
+import "./ViewcartPage.css"
 const ViewcartPage = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+        };
   return (
-    <div>ViewcartPage</div>
+    <>
+    <header className="VCheader">
+    <div className="VCoverlay-box">
+    <div className="VClogo">
+        CAT Pharmacy
+    </div>
+    <nav className={`VCmain-nav ${isMenuOpen ? 'open' : ''}`}>
+        <ul>
+            <li><a href="/LandingPage">Back to main</a></li>
+        </ul>
+    </nav>
+    <button className="VChamburger" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+    </button>
+    </div>
+</header>
+    </>
   )
 }
+
+
 
 export default ViewcartPage
